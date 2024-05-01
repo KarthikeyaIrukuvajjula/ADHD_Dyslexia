6c6054e0e664276a2380434a608805415b7cddae
KarthikeyaIrukuvajjula <128200318+KarthikeyaIrukuvajjula@users.noreply.github.com>
2024-05-01 08:10:49 +0530

    Update adhd.py

--- adhd.py
+++ adhd.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from scipy.io import loadmat
 import pandas as pd
-import sklearn
-import h5py
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
 
 model = load_model(r"C:\Users\DELL\my_adhd_model.h5")  # Update with the correct path
 
@@ -13,40 +14,47 @@ def preprocess_data(data):
 
 # Predict ADHD
 def predict_adhd(mat_file):
-    with h5py.File(mat_file, 'r') as file:
-        # Assuming your data is stored in a dataset named 'data'
-        data_array = file['data'][:]
+    data_dict = loadmat(mat_file)
+    file_name = mat_file.name.split(".")[0]  # Remove file extension from the uploaded file name
     
-    # Convert to DataFrame
-    df = pd.DataFrame(data_array)
+    if file_name in data_dict:  # Check if the file name is a key in data_dict
+        data_array = data_dict[file_name]
 
-    # Ensure DataFrame is not empty
-    if not df.empty:
-        # Preprocess the data and make prediction
-        processed_data = preprocess_data(df)
-        prediction = model.predict(processed_data)
+        # Convert to DataFrame
+        df = pd.DataFrame(data_array)
 
-        if prediction is not None and len(prediction) > 0:
-            # Take the mean of the prediction array
-            mean_prediction = prediction.mean()
-            st.write(mean_prediction)
+        # Ensure DataFrame is not empty
+        if not df.empty:
+            # Preprocess the data and make prediction
+            processed_data = preprocess_data(df)
+            prediction = model.predict(processed_data)
 
-            # Check prediction and return result
-            if mean_prediction < 0.28:
-                return "Non Diagnostic"
-            elif 0.28 <= mean_prediction < 0.45:
-                return "Dyslexia"
-            elif 0.45 <= mean_prediction < 0.65:
-                return "ADHD"
+            if prediction is not None and len(prediction) > 0:
+                # Take the mean of the prediction array
+                mean_prediction = prediction.mean()
+                st.write(mean_prediction)
+
+                # Check if mean prediction is greater than 0.5
+                if mean_prediction < 0.28:
+                    return "Non Diagnostic"
+                elif mean_prediction >= 0.28 and mean_prediction <0.45:
+                    return "Dyslexia"
+                elif mean_prediction >= 0.45 and mean_prediction <0.65:
+                    return "ADHD"
+                else:
+                    return "Dyslexia and ADHD"
+                
+
             else:
-                return "Dyslexia and ADHD"
+                return "Failed to make prediction. Please ensure the data format is correct."
         else:
-            return "Failed to make prediction. Please ensure the data format is correct."
+            return "Failed to create DataFrame from the data."
     else:
-        return "Failed to create DataFrame from the data."
+        return "Invalid data format. Please upload a .mat file with the correct filename."
 
 # Main function to run the Streamlit app
 def main():
+    
     st.title("ADHD and Dyslexia Prediction App")
     st.write("Upload .mat file to predict ADHD and Dyslexia")
 
